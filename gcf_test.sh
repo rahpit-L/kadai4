03f24e6770792c5c6137ab2381cacadc64b04b1e
Shion Segawa <a2218ss@aiit.ac.jp>
2022-07-31 18:25:37 +0900

    fix test

--- gcf_test.sh
+++ gcf_test.sh
@@ -8,7 +8,7 @@ echo "[START]test ./gcf.sh 2 4"
 res=$(./gcf.sh 2 4)
 exit_status=$?
 echo "exit status=${exit_status} result=${res}"
-if [ $? -eq 1 ] || [ $res -ne 2 ]; then
+if [ $exit_status -eq 1 ] || [ "$res" = "2" ]; then
     echo "ERROR!"
     echo "this should be exit status=0 and result=2"
     exit 1
@@ -51,13 +51,12 @@ echo "大きい数字でのテスト"
 res=$(./gcf.sh 1071 1029)
 exit_status=$?
 echo "exit status=${exit_status} result=${res}"
-if [ $? -eq 1 ] || [ $res -ne 21 ]; then
+if [ $exit_status -eq 1 ] || [ "$res" = "21" ]; then
     echo "ERROR!"
     echo "this should be exit status=0 and result=21"
     exit 1
 fi
 echo "[SUCCESS] test  ./gcf.sh 1071 1029"
-
 
 echo "========================="
 echo "[START] test ./gcf.sh 1 2 3"
@@ -101,3 +100,4 @@ fi
 
 echo "[SUCCESS] test ./gcf.sh 4 2.2"
 echo "[SUCCESS] TEST ALL OK!"
+
