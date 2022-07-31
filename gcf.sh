4e2ecb75576ed6b07dae4a29d42869e1f9e1f176
Shion Segawa <a2218ss@aiit.ac.jp>
2022-07-31 18:03:10 +0900

    initial commit

---
+++ gcf.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+a=$1
+b=$2
+extra=$3
+
+# arg check(natural number)
+if [ -z "$a" ] || ! [[ "$a" =~ ^[1-9][0-9]*$ ]]; then
+    echo "please input natural number as arg 1. ex) ./gcf.sh [natural number] [natural number]"
+    exit 1
+fi
+
+if [ -z "$b" ] || ! [[ "$b" =~ ^[1-9][0-9]*$ ]]; then
+    echo "please input natural number as arg 2. ex) ./gcf.sh [natural number] [natural number]"
+    exit 1
+fi
+
+if [ -n "$extra" ]; then
+    echo "please input only 2 natural number. ex) ./gcf.sh [natural number] [natural number]"
+    exit 1
+fi
+
+# start calculation
+c=$(($a % $b))
+while [ $c -ne 0 ]; do
+    tmp=$c
+    c=$(($b % $c))
+    b=$tmp
+done
+
+echo $b
+
---
+++ gcf_test.sh
@@ -0,0 +1,103 @@
+echo "========================="
+echo "running test for gcf.sh"
+
+# sample
+echo "========================="
+echo "2と4を入力する && 2を出力したら成功 || 出力しなかったらエラー"
+echo "[START]test ./gcf.sh 2 4"
+res=$(./gcf.sh 2 4)
+exit_status=$?
+echo "exit status=${exit_status} result=${res}"
+if [ $? -eq 1 ] || [ $res -ne 2 ]; then
+    echo "ERROR!"
+    echo "this should be exit status=0 and result=2"
+    exit 1
+fi
+
+echo "[SUCCESS] test ./gcf.sh 2 4"
+
+echo "========================="
+echo "[START] test ./gcf.sh 3"
+echo "3を入力する || エラーメッセージが出なかったらエラー"
+res=$(./gcf.sh 3)
+exit_status=$?
+echo "exit status=${exit_status} result=${res}"
+if [ $exit_status -eq 0 ]; then
+    echo "ERROR! exit status=${exit_status}"
+    echo "this should be exit status=1"
+    exit 1
+fi
+
+echo "[SUCCESS] test ./gcf.sh 3"
+
+echo "========================="
+echo "[START] test ./gcf.sh a a"
+echo "文字を入力する || エラーメッセージが出なかったらエラー"
+res=$(./gcf.sh a a)
+exit_status=$?
+echo "exit status=${exit_status} result=${res}"
+if [ $exit_status -eq 0 ]; then
+    echo "ERROR! "
+    echo "this should be exit status=1"
+    exit 1
+fi
+
+echo "[SUCCESS] test ./gcf.sh a a"
+
+# additional
+echo "========================="
+echo "[START] test ./gcf.sh 1071 1029"
+echo "大きい数字でのテスト"
+res=$(./gcf.sh 1071 1029)
+exit_status=$?
+echo "exit status=${exit_status} result=${res}"
+if [ $? -eq 1 ] || [ $res -ne 21 ]; then
+    echo "ERROR!"
+    echo "this should be exit status=0 and result=21"
+    exit 1
+fi
+echo "[SUCCESS] test  ./gcf.sh 1071 1029"
+
+
+echo "========================="
+echo "[START] test ./gcf.sh 1 2 3"
+echo "引数の数が多い"
+res=$(./gcf.sh 1 2 3)
+exit_status=$?
+echo "exit status=${exit_status} result=${res}"
+if [ $exit_status -eq 0 ]; then
+    echo "ERROR! "
+    echo "this should be exit status=1"
+    exit 1
+fi
+
+echo "[SUCCESS] test ./gcf.sh 1 2 3"
+
+echo "========================="
+echo "[START] test ./gcf.sh -1 2"
+echo "数値がマイナス"
+res=$(./gcf.sh -1 2)
+exit_status=$?
+echo "exit status=${exit_status} result=${res}"
+if [ $exit_status -eq 0 ]; then
+    echo "ERROR! "
+    echo "this should be exit status=1"
+    exit 1
+fi
+
+echo "[SUCCESS] test ./gcf.sh -1 2"
+
+echo "========================="
+echo "[START] test ./gcf.sh 4 2.2"
+echo "数値が小数"
+res=$(./gcf.sh 4 2.2)
+exit_status=$?
+echo "exit status=${exit_status} result=${res}"
+if [ $exit_status -eq 0 ]; then
+    echo "ERROR! "
+    echo "this should be exit status=1"
+    exit 1
+fi
+
+echo "[SUCCESS] test ./gcf.sh 4 2.2"
+echo "[SUCCESS] TEST ALL OK!"
